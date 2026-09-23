70430ea90bf21a4c7999c2aaea42a35c2158ec24
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-23 22:24:20 +0300

    proof-1: add proof-1/set-render.js

---
+++ proof-1/set-render.js
@@ -0,0 +1,19 @@
+export const setRender = {
+  BasePanel: {
+    width: "100%",
+    minHeight: "100dvh",
+    padding: "24px"
+  },
+  SimplePanel: {
+    width: "100%",
+    maxWidth: "420px",
+    padding: "24px"
+  },
+  ActivePanel: {
+    width: "100%",
+    padding: "24px"
+  },
+  Container: {
+    textAlign: "center"
+  }
+};
