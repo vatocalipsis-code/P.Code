52f3958dc149c0ad011ffe947d6f0eea8dea1945
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-24 00:39:16 +0300

    proof-5: plane-code.js two BasePanel swipe proof

--- proof-1/plane-code.js
+++ proof-1/plane-code.js
@@ -1,22 +1,36 @@
-export const planeCode = {
-  type: "BasePanel",
-  id: "base",
-  children: [
-    {
-      type: "SimplePanel",
-      id: "simple",
-      children: [
-        {
-          type: "ActivePanel",
-          id: "active",
-          children: [
-            {
-              type: "Container",
-              id: "message",
-            }
-          ]
-        }
-      ]
-    }
-  ]
-};
+export const planeCode = [
+  {
+    type: "BasePanel",
+    id: "home",
+    children: [
+      {
+        type: "SimplePanel",
+        id: "home-simple",
+        children: [
+          {
+            type: "ActivePanel",
+            id: "home-active",
+            children: [{ type: "Container", id: "home-message" }]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    type: "BasePanel",
+    id: "menu",
+    children: [
+      {
+        type: "SimplePanel",
+        id: "menu-simple",
+        children: [
+          {
+            type: "ActivePanel",
+            id: "menu-active",
+            children: [{ type: "Container", id: "menu-message" }]
+          }
+        ]
+      }
+    ]
+  }
+];
