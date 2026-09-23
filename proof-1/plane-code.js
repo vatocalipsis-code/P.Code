33069a22ac6360b5bcc058f6ea06ab790e4206ed
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-23 22:24:12 +0300

    proof-1: add proof-1/plane-code.js

---
+++ proof-1/plane-code.js
@@ -0,0 +1,23 @@
+export const planeCode = {
+  type: "BasePanel",
+  id: "base",
+  children: [
+    {
+      type: "SimplePanel",
+      id: "simple",
+      children: [
+        {
+          type: "ActivePanel",
+          id: "active",
+          children: [
+            {
+              type: "Container",
+              id: "message",
+              content: "Text"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
