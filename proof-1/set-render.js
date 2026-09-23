e20eb049080b3919e955b9019c0e147f9c577caa
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-23 22:33:26 +0300

    proof-1: use PanelSpacing for proof-1/set-render.js

--- proof-1/set-render.js
+++ proof-1/set-render.js
@@ -1,19 +1,3 @@
 export const setRender = {
-  BasePanel: {
-    width: "100%",
-    minHeight: "100dvh",
-    padding: "24px"
-  },
-  SimplePanel: {
-    width: "100%",
-    maxWidth: "420px",
-    padding: "24px"
-  },
-  ActivePanel: {
-    width: "100%",
-    padding: "24px"
-  },
-  Container: {
-    textAlign: "center"
-  }
+  PanelSpacing: 20
 };
