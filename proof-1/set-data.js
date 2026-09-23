4b63a89ecab69f388da17141c0e749907d8e27ab
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-24 00:32:43 +0300

    proof-4: use local Image asset in SetData

--- proof-1/set-data.js
+++ proof-1/set-data.js
@@ -1,6 +1,6 @@
 export const setData = {
   message: {
     type: "Image",
-    value: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='320' height='120' viewBox='0 0 320 120'%3E%3Crect width='320' height='120' fill='%23d9d9d9'/%3E%3Ccircle cx='70' cy='60' r='32' fill='%23666666'/%3E%3Ctext x='120' y='68' font-family='system-ui' font-size='22' fill='%231f1f1f'%3EPlaneCode Image%3C/text%3E%3C/svg%3E"
+    value: "./proof-4-image.svg"
   }
 };
