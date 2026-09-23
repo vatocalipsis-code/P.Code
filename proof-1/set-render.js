c099af9d10ca7897d52a7df46cfd12bcb4bf5d9a
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-24 01:39:11 +0300

    cleanup: remove obsolete proof-1/set-render.js

--- proof-1/set-render.js
+++
@@ -1,9 +0,0 @@
-export const setRender = {
-  PanelSpacing: 20,
-  BackgroundColor: "#f2f2f2",
-  PanelColor: "#f2f2f2",
-  BorderColor: "#555555",
-  TextColor: "#1f1f1f",
-  PanelDepth: 4,
-  PanelDepthColor: "#666666"
-};
