f6a2b83ac3c8bddbee26b26d3f61a4cf1bf504ac
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-25 02:05:08 +0300

    trial: let Container layout tail consume available space

--- runtime/web-renderer.js
+++ runtime/web-renderer.js
@@ -234,7 +234,7 @@ function renderContainerSequence(containers, index, inheritedDirection, dataSet,
 
   if (index + 1 < containers.length) {
     const tail = renderContainerSequence(containers, index + 1, direction, dataSet, renderSet, parallaxNodes, ownerShadow, gap);
-    tail.style.flex = "0 0 auto";
+    tail.style.flex = "1 1 auto";\n    tail.style.minWidth = "0";\n    tail.style.minHeight = "0";
     wrapper.append(tail);
   }
   return wrapper;
