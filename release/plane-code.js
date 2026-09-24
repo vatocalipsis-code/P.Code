c0cf3e65929d214627028e709ddbf0544a265d4a
vatocalipsis-code <322210037+vatocalipsis-code@users.noreply.github.com>
2026-09-24 03:04:45 +0300

    feat: name every PlaneCode entity

--- release/plane-code.js
+++ release/plane-code.js
@@ -2,26 +2,40 @@ export const planeCode = [
   {
     type: "BasePanel",
     id: "surface-a",
+    name: "SurfaceA",
     children: [{
       type: "SimplePanel",
       id: "surface-a-simple",
+      name: "SurfaceASimple",
       children: [{
         type: "ActivePanel",
         id: "surface-a-active",
-        children: [{ type: "Container", id: "surface-a-message" }]
+        name: "SurfaceAActive",
+        children: [{
+          type: "Container",
+          id: "surface-a-message",
+          name: "SurfaceAMessage"
+        }]
       }]
     }]
   },
   {
     type: "BasePanel",
     id: "surface-b",
+    name: "SurfaceB",
     children: [{
       type: "SimplePanel",
       id: "surface-b-simple",
+      name: "SurfaceBSimple",
       children: [{
         type: "ActivePanel",
         id: "surface-b-active",
-        children: [{ type: "Container", id: "surface-b-message" }]
+        name: "SurfaceBActive",
+        children: [{
+          type: "Container",
+          id: "surface-b-message",
+          name: "SurfaceBMessage"
+        }]
       }]
     }]
   }
